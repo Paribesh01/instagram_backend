9a7948daa271a2fb44a73800281c976842e84401
Paribesh Nepal <nepalparibesh01@gmail.com>
2024-05-01 00:25:24 +0530

    added comment endpoints

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Req } from '@nestjs/common';
+import { CommentService } from './comment.service';
+import { Request } from 'express';
 
 @Controller('comment')
-export class CommentController {}
+export class CommentController {
+
+constructor(private readonly commentService:CommentService){}
+
+@Post("/posComment/:postId")
+async postComment(@Body()content:string,@Param("postId",ParseIntPipe)postId:number,@Req()request:Request){
+return this.commentService.postComment(postId,request["user"].sub,content)
+}
+
+@Get(":id")
+async getOneComment(@Param("id",ParseIntPipe)id:number){
+    return this.commentService.findOne(id)
+}
+@Patch(":postId")
+async updateComment(@Body()content:string,@Param("postId",ParseIntPipe)postId:number,@Req()request:Request){
+    return this.commentService.updateComment(postId,request["user"].sub,content)
+}
+@Delete(":postId")
+async removeComment(@Param("postId",ParseIntPipe)postId:number,@Req()request:Request){
+    return this.commentService.removeComment(postId,request["user"].sub)
+}   
+@Get("post/:postId")
+async commentFromPost (@Param("postId",ParseIntPipe)postId:number){
+return this.commentService.commentFromPost(postId)
+}
+
+
+
+}
